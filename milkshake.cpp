2b2852b9c54ad089480171afe8e1064a4bd2e942
Julia Lai <jwwlai@gmail.com>
2013-03-10 22:46:22 -0700

    Added milkshake.cpp

---
+++ milkshake.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void foo(string s) {
+	for (int i=0; i<10; i+) {
+		cout << s << " MILKSHAKES!!!!!" << endl;
+
+	}
+}
+
+int main() {
+	foo("AHHHHH");
+}
