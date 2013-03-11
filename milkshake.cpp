9f5074550f4215dc77e81efca9fa5688b3d30fcc
David Nuon <david@davidnuon.com>
2013-03-10 22:55:41 -0700

    Eyo

--- milkshake.cpp
+++ milkshake.cpp
@@ -5,10 +5,11 @@ using namespace std;
 void foo(string s) {
 	for (int i=0; i<10; i++) {
 		cout << s << ", EYO MILKSHAKE!!!!!" << endl;
-
+		cout << s << ", EYO MILKSHAKE!!!!!" << endl;
 	}
 }
 
 int main() {
+    cout << "There will be shakes of the milky kind, eyo hither" << endl;
 	foo("AHHHHH");
 }
